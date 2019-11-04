df4ce39e60d6be898cdabb0e8cc6edfba742b26a
melissacoy <coyphillips@gmail.com>
2019-11-03 21:12:38 -0500

    updated html and js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function onSubmit(){
   $(`.js-submit`).click(event => {
       event.preventDefault();
-      let breed = $(`.js-submit`).val();
-      getDog(breed);
+      clearValues();
+      let dogBreed = $(`.js-text`).val().toLowerCase();
+      getDog(dogBreed);
     });
 }
-function getDog(breed){
-    fetch('https://dog.ceo/api/breed/' + breed + '/images/random')
+function getDog(dogBreed){
+    fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
     .then(response=> response.json())
     .then(responseJson => {
         console.log(responseJson);
-        if(responseJson.status !='success'){
+        if(responseJson.status !=='success'){
             throw Error(responseJson.message);
         }
         else{
-            $(`.js-dogs`).html(`<img src"${responseJson.message}" alt="picture of ${breed}" class="results-img">`)
+            $(`.js-dogs`).html(`<img src"${responseJson.message}" alt="picture of ${dogBreed}" class="results-img">`)
         }
     })
     .catch(error => {
-        console.log("an error occurred " + error.message);
-        $(`.js-dogs`).html(`<p>an error occurred: = ${error.message}</p>`)
+        console.log("error:  " + error.message);
+        $(`.js-dogs`).html(`<p>error : = ${error.message}</p>`)
     });
+}
+function clearValues() {
+    userNum = "";
+    endpoint = "";
+    $(`.js-dogs`).html("");
 }
 $(onSubmit);
